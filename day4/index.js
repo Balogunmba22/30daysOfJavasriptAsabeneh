c5fb9de136c35e4ffe0606485d8cb2db4b0c288e
Balogunmba22 <Balogunmba22@gmail.com>
2022-08-23 18:57:46 +0100

    complete solutions

--- day4/index.js
+++ day4/index.js
@@ -122,4 +122,35 @@ if (day == "saturday" || day == "sunday") {
 }
 else console.log(`${day} is a working day`);
 
-//LEVEL 3
+//LEVEL 3 Write a program which tells the number of days in a month.
+ month = prompt("Enter a month: ").toLowerCase();
+let daysCount;
+
+// check if it is leap year 
+let d = new Date();
+let year = d.getFullYear();
+
+if (month == "february") {
+   if ((year % 4 == 0 && year % 100 != 0) ||    (year % 400 == 0)) { daysCount = 29; }
+   else daysCount = 28;
+} else { 
+switch(month){
+   case "january":
+   case "march":
+   case "may":
+   case "july":
+   case "august":
+   case "october":
+   case "december":
+      daysCount = 31;
+      break;
+      
+   case "april":
+   case "june":
+   case "september":
+   case "november":
+      daysCount = 30;
+      break;
+}
+}
+console.log(month, " has ", daysCount, " days");
